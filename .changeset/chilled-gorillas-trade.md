7dc898f4279dac10cd04e7b494c0a433641bdd60
Adam Bergman <adam@fransvilhelm.com>
2020-12-14 22:58:17 +0100

    Initial project setup (#1)
    * Create icon exports
    
    * Add changeset
    
    * Add tests
    
    * Make feather-icons a dev dependency
    
    * Setup CI pipelines
    
    * Add documentation
    
    * Include correct files
    
    * Fix ci

---
+++ .changeset/chilled-gorillas-trade.md
@@ -0,0 +1,7 @@
+---
+'@fransvilhelm/feather': major
+---
+
+Initial release
+
+This is the initial release of @fransvilhelm/feather.
---
+++ src/Icon.tsx
@@ -0,0 +1,82 @@
+import React, { forwardRef, createContext, useContext } from 'react';
+
+export interface IconProps {
+  baseline?: boolean;
+  children: React.ReactNode;
+}
+
+const styles: Record<'span' | 'svg' | 'baseline', React.CSSProperties> = {
+  span: {
+    position: 'relative',
+    display: 'inline-flex',
+    alignSelf: 'center',
+    width: '1em',
+    height: '1em',
+  },
+  svg: {
+    width: '1em',
+    height: '1em',
+    fill: 'none',
+    stroke: 'currentColor',
+    strokeWidth: 2,
+    strokeLinecap: 'round',
+    strokeLinejoin: 'round',
+  },
+  baseline: {
+    position: 'absolute',
+    bottom: '-0.150em',
+  },
+};
+
+export const Icon = forwardRef<HTMLSpanElement, IconProps>(
+  ({ baseline, children }, ref) => {
+    const ctx = useContext(IconContext);
+
+    return (
+      <span ref={ref} style={styles.span}>
+        <svg
+          aria-hidden
+          vectorEffect="non-scaling-stroke"
+          {...ctx.svgProps}
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          style={{
+            ...styles.svg,
+            ...(baseline ? styles.baseline : null),
+            ...ctx.svgStyle,
+          }}
+        >
+          {children}
+        </svg>
+      </span>
+    );
+  },
+);
+
+type OmittedProps =
+  | 'style'
+  | 'className'
+  | 'width'
+  | 'width'
+  | 'height'
+  | 'viewBox'
+  | 'xmlns';
+
+interface TIconContext {
+  svgProps: Omit<React.SVGProps<SVGSVGElement>, OmittedProps>;
+  svgStyle: React.CSSProperties;
+}
+
+const defaultContext: TIconContext = {
+  svgProps: {
+    vectorEffect: 'non-scaling-stroke',
+    'aria-hidden': true,
+  },
+  svgStyle: {},
+};
+
+const IconContext = createContext<TIconContext>(defaultContext);
+
+export const IconProvider = IconContext.Provider;
