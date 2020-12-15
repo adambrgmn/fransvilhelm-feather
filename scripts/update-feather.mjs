3d1bd751cea4521aaf27b67bea406a06b2476e5d
Adam Bergman <adam@fransvilhelm.com>
2020-12-15 08:39:06 +0100

    Update feather check workflow

--- scripts/update-feather.mjs
+++ scripts/update-feather.mjs
@@ -1,12 +1,9 @@
 import * as path from 'path';
 import { promises as fs } from 'fs';
 import { exec } from '@actions/exec';
-import * as github from '@actions/github';
 import * as core from '@actions/core';
 import axios from 'axios';
 import semver from 'semver';
-
-let githubToken = process.env.GITHUB_TOKEN;
 
 const relativePath = (...paths) => path.join(process.cwd(), ...paths);
 const readJson = async (filePath) => {
@@ -14,11 +11,6 @@ const readJson = async (filePath) => {
 };
 
 (async () => {
-  if (!githubToken) {
-    core.setFailed('Please add the GITHUB_TOKEN to the changesets action');
-    return;
-  }
-
   let currentFeather = await readJson(
     'node_modules/feather-icons/package.json',
   );
@@ -29,51 +21,23 @@ const readJson = async (filePath) => {
 
   let diff = semver.diff(currentFeather.version, feather.version);
   switch (diff) {
-    case 'patch':
     case 'premajor':
     case 'preminor':
     case 'prepatch':
     case 'prerelease':
       console.log('Patch or prerelease found. Ignoring.');
+      core.setOutput('feather-bumped', 'false');
       return;
     case null:
       console.log('No version bump detected. Ignoring.');
+      core.setOutput('feather-bumped', 'false');
       return;
     default:
+      console.log('Feather icons bumped, initiating version bump');
   }
 
-  let repo = `${github.context.repo.owner}/${github.context.repo.repo}`;
-  let branch = `feather-icons/${feather.version}`;
-  let octokit = github.getOctokit(githubToken);
-
-  let searchQuery = `repo:${repo}+state:open+head:${branch}+base:main`;
-  let searchResult = await octokit.search.issuesAndPullRequests({
-    q: searchQuery,
-  });
-
-  if (searchResult.data.items.length === 0) {
-    console.log('PR already up. Ignoring.');
-  }
-
-  console.log('Feather icons bumped, initiating version bump');
-
-  await exec('git', [
-    'config',
-    '--global',
-    'user.name',
-    `"github-actions[bot]"`,
-  ]);
-
-  await exec('git', [
-    'config',
-    '--global',
-    'user.email',
-    `"github-actions[bot]@users.noreply.github.com"`,
-  ]);
-
-  await exec('git', ['checkout', '-b', branch]);
-
-  await exec('yarn', ['add', '--dev', 'feather-icons', '@types/feather-icons']);
+  await exec('yarn', ['add', 'feather-icons']);
+  await exec('yarn', ['add', '--dev', '@types/feather-icons']);
 
   let changeset = `
 ---
@@ -82,26 +46,11 @@ const readJson = async (filePath) => {
 
 Bump feather-icons to version ${feather.version}.
 `;
+
   await fs.writeFile(
     relativePath('.changeset/feather.md'),
     changeset.trim() + '\n',
   );
 
-  await exec('git', ['add', '.']);
-  await exec('git', [
-    'commit',
-    '-m',
-    `Bump feather icons to version ${feather.version}`,
-    '--no-verify',
-  ]);
-
-  await exec('git', ['push', 'origin', `HEAD:${branch}`, '--force']);
-
-  await octokit.pulls.create({
-    base: 'main',
-    head: branch,
-    title: `Bump feather-icons to version ${feather.version}`,
-    body: `Bump feather icons to version ${feather.version}`,
-    ...github.context.repo,
-  });
+  core.setOutput('feather-bumped', 'true');
 })();
