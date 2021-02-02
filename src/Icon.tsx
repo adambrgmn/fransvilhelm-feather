5ab450fdd187909c0d86c949d90ef6b35b9fc8a6
Adam Bergman <adam@fransvilhelm.com>
2021-02-02 13:58:29 +0100

    Skip applying faulty vector effect

--- src/Icon.tsx
+++ src/Icon.tsx
@@ -37,7 +37,6 @@ export const Icon = forwardRef<
     <span ref={ref} style={styles.span}>
       <svg
         aria-hidden
-        vectorEffect="non-scaling-stroke"
         {...ctx.svgProps}
         xmlns="http://www.w3.org/2000/svg"
         width="24"
